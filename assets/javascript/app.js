a9d22dd79f21ea7308a22c47bde60cd1eebc0cd3
dicimafran <cimafran16@gmail.com>
2018-06-06 14:25:03 -0400

    wip

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+/* Pseudocoding
+    This triviagame presents questions for the user to answer within a time limit. 
+    Each click on an answer returns a value indicating whether the user's answer is true or false.
+
+    Inputs
+        User's selected answer out of 4 answers.
+    
+    Outputs
+        Trivia questions: 8
+        Trivia answers: 8
+        
+    Process
+        T/F values will be involved for sure, maybe make a class or id for T/F
+            
+
+        Time limit which starts by default. It might be better to have a start button.
+             var windowTimeout = setTimeout(function() {
+                alert("Alert #1. Muwahahahaa! U have no choice!");
+             }, 1000);
+
+
+
+
+*/
